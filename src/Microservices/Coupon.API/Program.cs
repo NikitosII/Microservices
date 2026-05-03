23c2abce3687c32a6875fe557c44954f1e2309fe
NikitosII <160042225+NikitosII@users.noreply.github.com>
2026-05-03 22:46:49 +0300

    API versioning
    add /api/v1/ prefix across Coupon microservice

--- src/Microservices/Coupon.API/Program.cs
+++ src/Microservices/Coupon.API/Program.cs
@@ -1,3 +1,4 @@
+using Asp.Versioning;
 using Coupon.API.Data;
 using Coupon.API.Services;
 using FluentValidation;
@@ -8,6 +9,12 @@ using EventBus.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+});
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 builder.Services.AddEndpointsApiExplorer();
